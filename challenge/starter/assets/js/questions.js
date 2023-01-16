96a2b26f56c3ccc53ac1a20d462b91446e014bd5
Ola <117322790+Great-kiola@users.noreply.github.com>
2023-01-16 22:12:55 +0000

    Add files via upload

---
+++ challenge/starter/assets/js/questions.js
@@ -0,0 +1,41 @@
+var allquestions = [{
+        question: "What is the full meaning of CSS",
+        choice1: "Cascading Style sheet",
+        choice2: "Canscading Silly Songs" ,
+        choice3: "Cream, Spice and Sauce" ,
+        choice4: "Caramel Shark Shotgun",
+        answer: 1
+    },
+    {
+        question: "What does API stands for?" ,
+        choice1: "Application Programming Interface" ,
+        choice2: "Accessible Programme Interface" ,
+        choice3: "Application Parents Information" ,
+        choice4: "Artificial Programming Instructions",
+        answer: 1
+    },
+    {
+        question: "What does PHP stands for?",
+        choice1: "Hypertext Programming",
+        choice2: "Hyperstand Platform" ,
+        choice3: "Hibernate Programs " ,
+        choice4: "Hypertext Preprocessor",
+        answer: 4 
+    },
+    {
+        question: "What does SQL stands for?",
+        choice1: "Several Query Language",
+        choice2: "Structured Query Language" ,
+        choice3: "Standard Questions Language" ,
+        choice4: "Severe Questions Language",
+        answer: 2
+    },
+    {
+        question: "What is 10 + 10 equals to?",
+        choice1: "10",
+        choice2: "200" ,
+        choice3: "20" ,
+        choice4: "100",
+        answer: 3
+    }
+];
